f33f3a1de15da781105e2b41d075af1e4ed96f91
marcush12 <marcos012santos@gmail.com>
2018-06-05 20:54:59 -0300

    OK lesson 23

--- webservice/routes/api.php
+++ webservice/routes/api.php
@@ -13,6 +13,11 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::post('/cadastro', function (Request $request) {
+    $data = $request->all();
+    return $data;
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
