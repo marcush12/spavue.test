0101e8654f21e0204ab2337f55d3797a5c68f3e2
marcush12 <marcos012santos@gmail.com>
2018-06-05 21:15:01 -0300

    OK lesson 28

--- webservice/routes/api.php
+++ webservice/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,9 +16,13 @@ use Illuminate\Http\Request;
 
 Route::post('/cadastro', function (Request $request) {
     $data = $request->all();
-    return $data;
+    $user = User::create([
+        'name' =>$data['name'],
+        'email' =>$data['email'],
+        'password' =>bcrypt($data['password'])
+    ]);
+    return $user;
 });
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
